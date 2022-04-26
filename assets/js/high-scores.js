affc0edab800706eee19be47944934dd5f1e8312
Mohammad Rahman <mbrahman@gmail.com>
2022-04-26 07:27:10 -0400

    added high scores history

--- assets/js/high-scores.js
+++ assets/js/high-scores.js
@@ -1,3 +1,28 @@
-const scores = {...localStorage}
+const getScores = () => {
+    const oldData = JSON.parse(localStorage.getItem('code-quiz'))
+    console.log(oldData)
+    // displayScores(oldData)
+    sortScores(oldData)
+}
 
-console.log(scores)
+const sortScores = (arr) => {
+    const sortedScores = arr.sort((a, b) => {
+        // > 0 sort b before a
+        // < 0 sort a before b
+        return b.score - a.score
+    })
+    displayScores(sortedScores)
+}
+
+const displayScores = (arr) => {
+
+    for (let i = 0; i < arr.length; i++) {
+        const scoreEl = document.createElement('li')
+        scoreEl.setAttribute('class', 'list-group-item-info')
+
+        scoreEl.innerHTML = `${arr[i].initials} - ${arr[i].score}`
+        document.querySelector('#high-scores-ol').appendChild(scoreEl)
+    }
+}
+
+getScores()
